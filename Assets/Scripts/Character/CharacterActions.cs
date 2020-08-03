b182c096b669521bb205c2dbdc225108f4d1ce38
John Asuncion <johnasuncion0304@gmail.com>
2020-08-03 19:18:34 +0800

    Reorganized files

--- Assets/Scripts/Character/CharacterActions.cs
+++ Assets/Scripts/Character/CharacterActions.cs
@@ -5,13 +5,24 @@ using UnityEngine;
 public class CharacterActions : MonoBehaviour
 {
     Character character;
-
+    public float rayDistance = 1;
     public Item itemInRange;
     public Station stationInRange;
+
+    RaycastHit2D[] hit;
+    Ray2D ray;
+
 
     private void Awake()
     {
         character = GetComponent<Character>();
+    }
+
+    private void Update()
+    {
+        RaycastHit2D[] hit;
+        ray = new Ray2D(transform.position, character.characterController.facingDirection);
+        hit = Physics2D.RaycastAll(ray.origin, ray.direction, rayDistance);
     }
 
     public enum Actions
@@ -34,5 +45,11 @@ public class CharacterActions : MonoBehaviour
     public void Insert(Item item, Station station)
     {
         character.RecordInsert(item, station);
+    }
+
+    public void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawRay(ray.origin, ray.direction * rayDistance);
     }
 }
