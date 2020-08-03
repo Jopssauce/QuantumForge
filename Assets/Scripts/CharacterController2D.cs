aa87a229ed61016f0593f8b690d02b1ef6719345
John Asuncion <johnasuncion0304@gmail.com>
2020-08-03 12:01:11 +0800

    Implemented basic player controls

--- Assets/Scripts/CharacterController2D.cs
+++ Assets/Scripts/CharacterController2D.cs
@@ -4,15 +4,53 @@ using UnityEngine;
 
 public class CharacterController2D : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    float maxVelocity = 5f;
+    [SerializeField]
+    float maxAcceleration = 5f;
+
+    float axisX;
+    float axisY;
+    [SerializeField]
+    Vector2 currentVelocity;
+    Vector2 targetVelocity;
+
+    Rigidbody2D rgBody2D;
+
+    private void Awake()
     {
-        
+        rgBody2D = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        Vector2 input = new Vector2();
+        input.x = axisX;
+        input.y = axisY;
+        input = Vector2.ClampMagnitude(input, 1f);
+
+        targetVelocity = input * maxVelocity;
+        currentVelocity = rgBody2D.velocity;
+
+        currentVelocity = Vector2.MoveTowards(currentVelocity, targetVelocity, maxAcceleration * Time.deltaTime);
+
+        rgBody2D.velocity = currentVelocity;
+    }
+
+    //public IEnumerator MoveRight()
+    //{
+    //    MoveHorizontal(1);
+    //    yield return new WaitForSeconds(1);
+    //    MoveHorizontal(0);
+    //}
+
+    public void MoveHorizontal(float axis)
+    {
+        axisX = axis;
+    }
+
+    public void MoveVeritcal(float axis)
+    {
+        axisY = axis;
     }
 }
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public CharacterController2D characterController2D;
+
+    private void Update()
+    {
+        characterController2D.MoveHorizontal(Input.GetAxis("Horizontal"));
+        characterController2D.MoveVeritcal(Input.GetAxis("Vertical"));
+    }
+}
