fe0ae487d56a059d9c27fca199482d4e7dee3e39
John Asuncion <johnasuncion0304@gmail.com>
2020-08-06 13:16:19 +0800

    Level Config

---
+++ Assets/Scripts/LevelConfig.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(fileName = "Level Config", menuName = "Level/Level Config", order = 1)]
+public class LevelConfig : ScriptableObject
+{
+    public float maxRecordTime = 5f;
+    public int recordAmount = 3;
+}
